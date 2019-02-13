baca3bf1a7853bfe062a379b7497d6e4260702f2
William S Berry <wsberry@gmail.com>
2019-02-13 15:08:29 -0600

    Added missing 'drag-drop-extensions.cs'.

---
+++ src/cs/source-filter/slx/drag-drop-extensions.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace slx
+{
+   public static class drag_drop_extensions
+   {
+      public static void DropFolder(this TextBox tb, DragEventArgs e)
+      {
+         var paths = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+         if (paths.Length <= 0 || string.IsNullOrEmpty(paths[0]) || !Directory.Exists(paths[0])) return;
+         tb.Text = paths[0];
+         tb.SelectionLength = 0;
+      }
+   }
+}
